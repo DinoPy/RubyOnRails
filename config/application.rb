5a774a0921ddc9540905b70cc986ff163dbc1ded
DinoPy <78703794+DinoPy@users.noreply.github.com>
2022-12-18 21:16:13 +0000

    added some style, and a new database option.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CodespacesBlank
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.web_console.permissions = '188.25.3.134'
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pins
+  devise_for :users
   root 'home#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
